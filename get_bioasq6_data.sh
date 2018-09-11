e0b6299dbf9ffec2688288c200a36d1294c412c1
gbrokos <g.brokos@gmail.com>
2018-09-11 03:08:42 +0300

    Update get_bioasq6_data.sh

--- get_bioasq6_data.sh
+++ get_bioasq6_data.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-mkdir -p bioasq_data
+mkdir -p bioasq6_data
 
-cd bioasq_data
+cd bioasq6_data
 
 wget -c https://archive.org/download/aueb_bioasq6_data/bioasq6_data.tar.gz
-tar -zxvf bioasq_data.tar.gz
+tar -zxvf bioasq6_data.tar.gz
 
 wget -c https://archive.org/download/biomedical_idf.tar/biomedical_idf.tar.gz
 tar -zxvf biomedical_idf.tar.gz
